df57b0034c5efe5db202e53cb23a37fe129d9b6c
Mario Lubenka <mario.lubenka@googlemail.com>
2020-01-28 23:47:32 +0100

    feat: provide binary

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import {Cli, HelpCommand, VersionCommand} from "@saithodev/cli-base";
 import {VersionizeCommand} from "./commands/VersionizeCommand";
 
-new Cli('typo3-extension-release')
-    .setOptions({
-        'dry-run': {
-            type: 'boolean',
-            default: false,
-            alias: 'd',
-            description: 'Simulates changes without writing them'
-        }
-    })
-    .addCommand(new VersionizeCommand())
-    .addCommand(new HelpCommand())
-    .addCommand(new VersionCommand())
-    .run()
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+export default function () {
+    new Cli('typo3-extension-release')
+        .setOptions({
+            'dry-run': {
+                type: 'boolean',
+                default: false,
+                alias: 'd',
+                description: 'Simulates changes without writing them'
+            }
+        })
+        .addCommand(new VersionizeCommand())
+        .addCommand(new HelpCommand())
+        .addCommand(new VersionCommand())
+        .run()
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
+}
